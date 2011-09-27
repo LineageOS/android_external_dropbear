9e693fabd54bd6082c95d765ea8fa907adc30c7b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-09-27 11:53:48 +0200

    android: treat wrong pty chmod as error, but continue
    something is wrong when dropbear is not executed as root
    it try to change uid:gid of pty char device to root:root
    
    ignore and log the error until fixes, but don't exit

--- sshpty.c
+++ sshpty.c
@@ -431,9 +431,18 @@ pty_setowner(struct passwd *pw, const char *tty_name)
 						tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid,
 						strerror(errno));
 			} else {
+#ifdef ANDROID_CHANGES
+				/* allow non-root system apps to login (reversed from/to uid:gids ?) */
+				dropbear_log(LOG_ERR,
+					"chown(%.100s, %u, %u) from %u:%u failed: %.100s",
+						tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid,
+						st.st_uid, st.st_gid,
+						strerror(errno));
+#else
 				dropbear_exit("chown(%.100s, %u, %u) failed: %.100s",
 				    tty_name, (unsigned int)pw->pw_uid, (unsigned int)gid,
 				    strerror(errno));
+#endif
 			}
 		}
 	}
